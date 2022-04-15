d47f6574229904688850e6926b245f78ebda8acc
Alexis De Leon <alexisdlr3009@hotmail.com>
2022-04-15 18:54:58 -0500

    🔥My portfolio web: updated

--- js/nav-responsive.js
+++ js/nav-responsive.js
@@ -1,7 +1,19 @@
 const primaryNav = document.querySelector('#navigation');
 const menuBtn = document.querySelector('.menu-btn');
+const li = document.querySelector('.li-1');
+const li2 = document.querySelector('.li-2');
+const li3 = document.querySelector('.li-3');
+const li4 = document.querySelector('.li-4');
+const li5 = document.querySelector('.li-5');
 
 menuBtn.addEventListener('click', toggleMenu);
+li.addEventListener('click', toggleMenu);
+li2.addEventListener('click', toggleMenu);
+li3.addEventListener('click', toggleMenu);
+li4.addEventListener('click', toggleMenu);
+li5.addEventListener('click', toggleMenu);
+
+
 function toggleMenu (e) {
     const visibility = primaryNav.getAttribute("data-visible");
     if (visibility === "false"){
