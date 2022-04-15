01404c438d41f6d7f7038d7da4f6869dc55be57f
Alexis De Leon <alexisdlr3009@hotmail.com>
2022-04-15 12:36:34 -0500

    🔥My portfolio web

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -1,11 +1,11 @@
-ScrollReveal().reveal('.welcome-section', { duration: 2000, origin: 'bottom', distance: '-180px' });
-ScrollReveal().reveal('.img-container__a', { duration: 2000, origin: 'left', distance: '-180px' });
+ScrollReveal().reveal('.welcome-section', { duration: 2000, origin: 'bottom', distance: '-400px' });
+ScrollReveal().reveal('.img-container__a', { duration: 2000, origin: 'left', distance: '-330px' });
 ScrollReveal().reveal('.name-title', { duration: 2000, origin: 'left', distance: '180px' });
 ScrollReveal().reveal('.aka', { duration: 2000, origin: 'left', distance: '-180px' });
 ScrollReveal().reveal('.dev', { duration: 2000, origin: 'top', distance: '-180px' });
 ScrollReveal().reveal('.projects', { duration: 2000, origin: 'left', distance: '-180px' });
 ScrollReveal().reveal('.projects-p', { duration: 2000, origin: 'left', distance: '200px' });
-ScrollReveal().reveal('.grid-item', { duration: 3000, origin: 'bottom', distance: '180px' });
+ScrollReveal().reveal('.work', { duration: 3000, origin: 'bottom', distance: '180px' });
 ScrollReveal().reveal('.conect', { duration: 3000, origin: 'top', distance: '180px' });
 ScrollReveal().reveal('.form', { duration: 3000, origin: 'bottom', distance: '180px' });
 ScrollReveal().reveal('.footer', { duration: 2000, origin: 'top', distance: '180px' });
