bf3b52f1922755e5fc2592e58b120a43b476ef1e
Alexis De Leon <alexisdlr3009@hotmail.com>
2022-04-14 19:50:17 -0500

    🔥My portfolio web

---
+++ js/scrollreveal.js
@@ -0,0 +1,13 @@
+ScrollReveal().reveal('.welcome-section', { duration: 2000, origin: 'bottom', distance: '-180px' });
+ScrollReveal().reveal('.img-container__a', { duration: 2000, origin: 'left', distance: '-180px' });
+ScrollReveal().reveal('.navigation__ul', { duration: 2000, origin: 'left', distance: '-180px' });
+ScrollReveal().reveal('.name-title', { duration: 2000, origin: 'left', distance: '180px' });
+ScrollReveal().reveal('.aka', { duration: 2000, origin: 'left', distance: '-180px' });
+ScrollReveal().reveal('.dev', { duration: 2000, origin: 'top', distance: '-180px' });
+ScrollReveal().reveal('.projects', { duration: 2000, origin: 'left', distance: '-180px' });
+ScrollReveal().reveal('.projects-p', { duration: 2000, origin: 'left', distance: '200px' });
+ScrollReveal().reveal('.grid-item', { duration: 3000, origin: 'bottom', distance: '180px' });
+ScrollReveal().reveal('.conect', { duration: 3000, origin: 'top', distance: '180px' });
+ScrollReveal().reveal('.form', { duration: 3000, origin: 'bottom', distance: '180px' });
+
+
